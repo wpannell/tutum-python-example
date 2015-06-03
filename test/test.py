fb84b80b10d41dbbeeca92e08a084118cc0e9e63
Jack Firth <jackhfirth@gmail.com>
2015-06-02 20:32:43 -0700

    Fix deletion and extend test

--- test/test.py
+++ test/test.py
@@ -1,8 +1,10 @@
 import unirest
 
 
-response = unirest.get('http://web:5000')
+def assert_response(expected_response_body):
+    response = unirest.get('http://web:5000')
+    assert response.code == 200
+    assert response.body == expected_response_body
 
-assert response.code == 200
-
-assert response.body == "Hello World! I have been seen 1 times."
+assert_response("Hello World! I have been seen 1 times.")
+assert_response("Hello World! I have been seen 2 times.")
